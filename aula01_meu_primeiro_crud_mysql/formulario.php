80511a9d9dbb87a0e54791164c95ffac66396027
Cristiano Aroxa <112488905+CristianoAroxa@users.noreply.github.com>
2023-03-29 22:38:47 -0300

    Update formulario.php

--- aula01_meu_primeiro_crud_mysql/formulario.php
+++ aula01_meu_primeiro_crud_mysql/formulario.php
@@ -40,6 +40,75 @@ if(count($_POST)>0){
     }
 }
    
-?>    
+?> 
+    
+  
+
+<form method="POST">
+	<div class="form group row">
+		<div class="form-group col-md-8">
+			<label for="nome">Nome: </label>
+			<input type="text" class="form-control" <?php echo isset($erros['nome'])?'is-invalid':''?>"
+			id="id_nome" name="nome" placeholder="Digite o nome..." value="<?php echo isset($_POST['nome'])? $_POST['nome']:''?>">
+			<div class="invalid-feedback">
+				<?php echo $erros['nome']; ?>
+			</div>
+		</div>
+	</div>
+	
+	<div class="form-group col-md-4>
+		<label for="nascimento">Nascimento:</label>
+		<input type="text" class="form-control" <?php echo isset($erros['nascimento'])?'is-invalid':''?>"
+		id="nascimento" name="nascimento" placeholder="Nascimento" value="<?php echo isset($_POST['nascimento'])? $_POST['nascimento']:''?>">
+		<div class="invalid-feedback">
+			<?php echo $erros['nacimento'];?>
+		</div>
+	</div>
+	
+	<div class="form group row">
+		<div class="form-group col-md-8">
+			<label for="email">E-mail: </label>
+			<input type="text" class="form-control" <?php echo isset($erros['email'])?'is-invalid':''?>"
+			id="id_email" name="email" placeholder="Digite o email..." value="<?php echo isset($_POST['email'])? $_POST['email']:''?>">
+			<div class="invalid-feedback">
+				<?php echo $erros['email']; ?>
+			</div>
+		</div>
+	</div>
+	
+	<div class="form-group col-md-4>
+		<label for="site">Site:</label>
+		<input type="text" class="form-control" <?php echo isset($erros['site'])?'is-invalid':''?>"
+		id="site" name="site" placeholder="site" value="<?php echo isset($_POST['site'])? $_POST['site']:''?>">
+		<div class="invalid-feedback">
+			<?php echo $erros['site'];?>
+		</div>
+	</div>
+	
+	<div class="form group row">
+		<div class="form-group col-md-8">
+			<label for="filhos">Filhos: </label>
+			<input type="text" class="form-control" <?php echo isset($erros['filhos'])?'is-invalid':''?>"
+			id="id_filhos" name="filhos" placeholder="Digite o numero de filhos..." value="<?php echo isset($_POST['filhos'])? $_POST['filhos']:''?>">
+			<div class="invalid-feedback">
+				<?php echo $erros['filhos']; ?>
+			</div>
+		</div>
+	</div>
+	
+	<div class="form-group col-md-4>
+		<label for="salario">Salário:</label>
+		<input type="text" class="form-control" <?php echo isset($erros['salario'])?'is-invalid':''?>"
+		id="salario" name="salario" placeholder="Digite o salário" value="<?php echo isset($_POST['salario'])? $_POST['salario']:''?>">
+		<div class="invalid-feedback">
+			<?php echo $erros['salario'];?>
+		</div>
+	</div>
+	
+	<div class="mb-3">
+		<button class="btn btn-primary btn-leg">Enviar</button>
+	</div>
+	
+</form>	
 </body>
 </html>
